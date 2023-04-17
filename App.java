f05b59abe4ec8f9e5d52c4ad835129e6fc8de5cb
kpxics <120198148+kpxics@users.noreply.github.com>
2023-04-17 01:36:10 -0400

    App.java

---
+++ App.java
@@ -0,0 +1,17 @@
+package com.h2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int doubleTheNumber(int number) {
+        return 2 * number;
+    }
+}
